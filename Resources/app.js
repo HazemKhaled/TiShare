b592a46313cc0f61a1bd0228f9df2fff82637f37
Hazem Khaled <hazem.khaled@gmail.com>
2012-02-12 01:12:09 +0200

    example

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,29 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
 //
 // create base UI tab and root window
 //
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+var win1 = Titanium.UI.createWindow({
+	title : 'Tab 1',
+	backgroundColor : '#fff'
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var Tishare = require('lib/tishare');
+
+Tishare.services({
+	SMS : true
 });
 
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
+var shareBtn = Tishare.btn(Ti.UI.createButton({
+	title : 'Share'
+}), {
+	text : 'Appcelerator is god mof fod http://appcelerator',
+	mailSubject : 'use appcelerator',
+	mailBody : 'more egyptian developers use appcelerator \n and appcelerator is tmam',
+	smsBody : 'appcelerator smsm'
 });
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
+win1.add(shareBtn);
 
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+win1.open();
